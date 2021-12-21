fc891410f486d5b3a12257cafa5f313e2160b400
Klas Segeljakt <klasseg@kth.se>
2021-12-21 10:44:38 +0100

    arc-lang: Add Arc-Lang

--- arc-mlir/src/tests/lit.cfg.py
+++ arc-mlir/src/tests/lit.cfg.py
@@ -52,6 +52,14 @@ config.test_exec_root = os.path.join(config.mlir_obj_root, 'test')
 # Tweak the PATH to include the tools dir.
 llvm_config.with_environment('PATH', config.llvm_tools_dir, append_path=True)
 
+from pathlib import Path
+
+# /path/to/arc/arc-mlir/src/tests/lit.cfg.py => /path/to/arc/arc-lang/stdlib/
+arc_stdlib_dir = Path(__file__).parents[3] / 'arc-lang' / 'stdlib' 
+
+llvm_config.with_environment('ARC_LANG_STDLIB_PATH', arc_stdlib_dir / 'stdlib.arc')
+llvm_config.with_environment('ARC_MLIR_STDLIB_PATH', arc_stdlib_dir / 'stdlib.mlir')
+
 tool_dirs = [config.mlir_tools_dir, config.llvm_tools_dir,
              config.arcscript_tools_dir]
 tools = [
