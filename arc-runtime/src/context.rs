f96c358e711fbd17538fddb8799815697f4f0e3d
Klas Segeljakt <klasseg@kth.se>
2022-01-25 14:38:05 +0100

    Update arc-runtime

--- arc-runtime/src/context.rs
+++ arc-runtime/src/context.rs
@@ -4,15 +4,8 @@ use comet::mutator::MutatorRef;
 use kompact::prelude::*;
 
 pub struct Context {
-    pub system: System,
+    pub system: KompactSystem,
     pub mutator: MutatorRef<MiniMark>,
-}
-
-pub type System = impl kompact::prelude::SystemHandle;
-
-fn get_handle<T: Actor + ComponentDefinition>(c: &ComponentDefinitionAccess<T>) -> impl kompact::prelude::SystemHandle {
-    let x =c.ctx().system();
-    x
 }
 
 impl Context {
@@ -21,16 +14,5 @@ impl Context {
         let system = KompactConfig::default().build().unwrap();
         let mutator = comet::minimark::instantiate_minimark(opts);
         Self { system, mutator }
-    }
-}
-
-impl<T: Actor + ComponentDefinition> From<kompact::prelude::ComponentContext<T>> for Ctx<T> {
-    fn from(ctx: kompact::prelude::ComponentContext<T>) -> Self {
-        let opts = MiniMarkOptions::default();
-        let mutator = comet::minimark::instantiate_minimark(opts);
-        Self {
-            kompact: ctx,
-            mutator,
-        }
     }
 }
